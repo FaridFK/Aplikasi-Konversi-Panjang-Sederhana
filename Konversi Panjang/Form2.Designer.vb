184f17c0b3c2c4744f83aaffc40d6163e3d0d192
FaridFK <74176005+FaridFK@users.noreply.github.com>
2022-06-22 22:18:51 +0700

    Add files via upload

---
+++ Konversi Panjang/Form2.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnok = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(32, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(381, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PROGRAM KONVERSI PANJANG"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.White
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 54)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(383, 115)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Konversi"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.White
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox2.Location = New System.Drawing.Point(213, 185)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 70)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Hasil"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.White
+        Me.ListBox1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Items.AddRange(New Object() {"1. Kilometer", "2. Hektometer", "3. Dekameter", "4. Meter", "5. Desimeter", "6. Centimeter", "7. Milimeter"})
+        Me.ListBox1.Location = New System.Drawing.Point(30, 185)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(158, 204)
+        Me.ListBox1.TabIndex = 3
+        '
+        'btnok
+        '
+        Me.btnok.BackColor = System.Drawing.Color.White
+        Me.btnok.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnok.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnok.Location = New System.Drawing.Point(276, 270)
+        Me.btnok.Name = "btnok"
+        Me.btnok.Size = New System.Drawing.Size(75, 31)
+        Me.btnok.TabIndex = 4
+        Me.btnok.Text = "OK"
+        Me.btnok.UseVisualStyleBackColor = False
+        '
+        'btnclear
+        '
+        Me.btnclear.BackColor = System.Drawing.Color.White
+        Me.btnclear.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnclear.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnclear.Location = New System.Drawing.Point(276, 321)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(75, 31)
+        Me.btnclear.TabIndex = 5
+        Me.btnclear.Text = "CLEAR"
+        Me.btnclear.UseVisualStyleBackColor = False
+        '
+        'btnexit
+        '
+        Me.btnexit.BackColor = System.Drawing.Color.White
+        Me.btnexit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnexit.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnexit.Location = New System.Drawing.Point(276, 369)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(75, 31)
+        Me.btnexit.TabIndex = 6
+        Me.btnexit.Text = "EXIT"
+        Me.btnexit.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(136, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Masukkan Bilangan"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(183, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(166, 27)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Dari"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(67, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(75, 27)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(242, 77)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Ke"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(274, 74)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(75, 27)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(22, 25)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(160, 27)
+        Me.TextBox4.TabIndex = 0
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(442, 425)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnok)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form2"
+        Me.Text = "Konversi Panjang"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnok As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents btnexit As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ Konversi Panjang/Form2.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form2
+    Dim a As Double
+    Dim b As Double
+    Dim tutup As String
+    Private Sub btnok_Click(sender As Object, e As EventArgs) Handles btnok.Click
+        a = TextBox2.Text
+        b = TextBox3.Text
+
+        If a = 1 Then
+            If b = 2 Then
+                'Kilometer ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            ElseIf b = 3 Then
+                'Kilometer ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) * 100
+            ElseIf b = 4 Then
+                'Kilometer ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) * 1000
+            ElseIf b = 5 Then
+                'Kilometer ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10000
+            ElseIf b = 6 Then
+                'Kilometer ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 100000
+            ElseIf b = 7 Then
+                'Kilometer ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 1000000
+            End If
+
+        ElseIf a = 2 Then
+            If b = 1 Then
+                'Hektometer ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            ElseIf b = 3 Then
+                'Hektometer ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            ElseIf b = 4 Then
+                'Hektometer ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) * 100
+            ElseIf b = 5 Then
+                'Hektometer ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 1000
+            ElseIf b = 6 Then
+                'Hektometer ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10000
+            ElseIf b = 7 Then
+                'Hektometer ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 100000
+            End If
+
+        ElseIf a = 3 Then
+            If b = 1 Then
+                'Dekameter ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 100
+            ElseIf b = 2 Then
+                'Dekameter ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            ElseIf b = 4 Then
+                'Dekameter ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            ElseIf b = 5 Then
+                'Dekameter ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 100
+            ElseIf b = 6 Then
+                'Dekameter ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 1000
+            ElseIf b = 7 Then
+                'Dekameter ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10000
+            End If
+
+        ElseIf a = 4 Then
+            If b = 1 Then
+                'Meter ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 1000
+            ElseIf b = 2 Then
+                'Meter ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) / 100
+            ElseIf b = 3 Then
+                'Meter ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            ElseIf b = 5 Then
+                'Meter ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            ElseIf b = 6 Then
+                'Meter ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 100
+            ElseIf b = 7 Then
+                'Meter ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 1000
+            End If
+
+        ElseIf a = 5 Then
+            If b = 1 Then
+                'Desimeter ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 10000
+            ElseIf b = 2 Then
+                'Desimeter ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) / 1000
+            ElseIf b = 3 Then
+                'Desimeter ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) / 100
+            ElseIf b = 4 Then
+                'Desimeter ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            ElseIf b = 6 Then
+                'Desimeter ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            ElseIf b = 7 Then
+                'Desimeter ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 100
+            End If
+
+        ElseIf a = 6 Then
+            If b = 1 Then
+                'Centimeter ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 100000
+            ElseIf b = 2 Then
+                'Centimeter ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) / 10000
+            ElseIf b = 3 Then
+                'Centimeter ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) / 1000
+            ElseIf b = 4 Then
+                'Centimeter ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) / 100
+            ElseIf b = 5 Then
+                'Centimeter ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            ElseIf b = 7 Then
+                'Centimeter ke Milimeter'
+                TextBox4.Text = Val(TextBox1.Text) * 10
+            End If
+
+        ElseIf a = 7 Then
+            If b = 1 Then
+                'Milimeter ke Kilometer'
+                TextBox4.Text = Val(TextBox1.Text) / 1000000
+            ElseIf b = 2 Then
+                'Milimeter ke Hektometer'
+                TextBox4.Text = Val(TextBox1.Text) / 100000
+            ElseIf b = 3 Then
+                'Milimeter ke Dekameter'
+                TextBox4.Text = Val(TextBox1.Text) / 10000
+            ElseIf b = 4 Then
+                'Milimeter ke Meter'
+                TextBox4.Text = Val(TextBox1.Text) / 1000
+            ElseIf b = 5 Then
+                'Milimeter ke Desimeter'
+                TextBox4.Text = Val(TextBox1.Text) / 100
+            ElseIf b = 6 Then
+                'Milimeter ke Centimeter'
+                TextBox4.Text = Val(TextBox1.Text) / 10
+            End If
+        End If
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        tutup = MessageBox.Show("Apakah anda ingin keluar ?", "Peringatan", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If tutup = MsgBoxResult.Yes Then
+            End
+        Else
+        End If
+    End Sub
+End Class
